0377b32f7c0e967cc06328b98517b1f07af1e6bd
Jonny Zhou <jonny.zhou@oocl.com>
2025-09-12 15:03:10 +0800

    feat: apply style to todo item

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,16 @@
+import {useContext} from "react";
+import {TodoContext} from "../contexts/TodoContext";
+
 const TodoList = () => {
-  return (
-      <div>This is the TodoList Component.</div>
-  );
+    const {state, dispatch} = useContext(TodoContext)
+    return <div>
+        <div>This is the TodoList Component.</div>
+        {
+            state.map(todoItem => {
+                return <div style={{width:"200px",border:"1px solid black",padding:"10px",marginBottom:"10px",justifySelf:"center"}}>{todoItem.text}</div>
+            })
+        }
+    </div>
 }
 
 export default TodoList
