7731f8c93c0440d6fce13aaae2e29853a9828f88
Jonny Zhou <jonny.zhou@oocl.com>
2025-09-12 15:31:13 +0800

    feat: add strikethrough style to todo item if it has been done

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,11 +3,16 @@ import {TodoContext} from "../contexts/TodoContext";
 
 const TodoList = () => {
     const {state, dispatch} = useContext(TodoContext)
+
+    function handleItemClick(id) {
+        dispatch({type:"DONE",id})
+    }
+
     return <div>
         <div>This is the TodoList Component.</div>
         {
-            state.map(todoItem => {
-                return <div style={{width:"200px",border:"1px solid black",padding:"10px",marginBottom:"10px",justifySelf:"center"}}>{todoItem.text}</div>
+            state.map(({text,done,id}) => {
+                return <div onClick={() => handleItemClick(id)} style={{width:"200px",border:"1px solid black",padding:"10px",marginBottom:"10px",justifySelf:"center",textDecoration:done? "line-through" : "auto"}}>{text}</div>
             })
         }
     </div>
--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -1,9 +1,18 @@
 export const initialState = [
-  {id: 1, text: "the first todo", done: false},
-  {id: 2, text: "the second todo", done: false},
+    {id: 1, text: "the first todo", done: false},
+    {id: 2, text: "the second todo", done: false},
 ];
 
 // reducer is a pure function that define and gather all state update logic
 export const todoReducer = (state, action) => {
-  return state;
+    if (action.type === "DONE") {
+        return state.map(item => {
+            if (item.id === action.id) {
+                return {...item, done: !item.done}
+            }
+            return item
+        })
+        console.log(JSON.stringify(state))
+    }
+    return state;
 };
